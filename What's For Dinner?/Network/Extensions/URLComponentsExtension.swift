575e00ae93c928d78b905348139c25ac8bdde063
Isaac Delgado <idelgado@phunware.com>
2019-09-25 11:05:50 -0500

    - Network layer: 	- Added extensions for URLRequest and URLComponents

---
+++ What's For Dinner?/Network/Extensions/URLComponentsExtension.swift
@@ -0,0 +1,23 @@
+//
+//  URLComponentsExtension.swift
+//  What's For Dinner?
+//
+//  Created by Isaac Delgado on 9/25/19.
+//  Copyright © 2019 Isaac Delgado. All rights reserved.
+//
+
+import Foundation
+
+extension URLComponents {
+    
+    init(service: ServiceProtocol) {
+        let url = service.baseURL.appendingPathComponent(service.path)
+        self.init(url: url, resolvingAgainstBaseURL: false)!
+        
+        guard case let .requestParameters(parameters) = service.task, service.parametersEncoding == .url else { return }
+        
+        queryItems = parameters.map { key, value in
+            return URLQueryItem(name: key, value: String(describing: value))
+        }
+    }
+}
